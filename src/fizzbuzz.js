58a86a5a7d6fd1e911f0e03d52575073da4a62d9
James Byrne <james.c.byrne1994@gmail.com>
2022-07-13 13:24:16 +0100

    completed fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,42 @@
 // TODO: Write your function in this file
 
+// function theFizzBuzzTest (lower, upper){
+//     const answer = []
+
+//     for(let i = lower; i <= upper; i++ ) {
+//        if (i % 3 === 0 && i % 5 === 0) {
+//         answer.push("FizzBuzz")
+//        } else if (i % 3 === 0){
+//         answer.push("Fizz")
+//        } else if(i % 5 === 0) {
+//         answer.push("Buzz")
+//        } else {
+//         answer.push(i)
+//        }
+//        return answer
+//     }
+    
+// }
+
+function theFizzBuzzTest (lower, upper){
+    const answer = []
+ 
+    for(let i =lower; i <= upper; i++){
+        if(i % 3 === 0 && i % 5 === 0 ){
+            answer.push("FizzBuzz")  
+        } else if (i % 3 === 0){
+            answer.push("Fizz")
+        } else if (i % 5 === 0){
+        answer.push("Buzz")  
+        }  else {
+            answer.push(i)
+        }
+    }
+    return answer
+
+}
+
+
+
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = theFizzBuzzTest
